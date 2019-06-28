b856ba537c37f1f3d1e36e611dd4d94fa0bae67d
Timbo <blaesche.m@gmail.com>
2019-06-28 10:43:57 +0200

    Adds the new component to the router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,31 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import store from "./store";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import SingleplayerView from "./container/SingleplayerView";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: "#f7b731"
+		},
+		secondary: {
+			main: "#eb3b5a"
+		},
+	},
+	typography: {
+		fontFamily: ["Arimo", "sans-serif"].join(",")
+	}
+});
 
 ReactDOM.render(
 	<Provider store={store}>
-		<BrowserRouter>
-			<Route exact={true} path={"/"} component={App}/>
-		</BrowserRouter>
+		<MuiThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Route exact={true} path={"/"} component={App}/>
+				<Route exact={true} path={"/singleplayer"} component={SingleplayerView}/>
+			</BrowserRouter>
+		</MuiThemeProvider>
 	</Provider>,
 	document.getElementById('root'));
 registerServiceWorker();
