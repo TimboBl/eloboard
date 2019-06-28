41074931c91404e72081eed7a7b405f5c21953d5
Timbo <blaesche.m@gmail.com>
2019-06-28 14:49:43 +0200

    Changes the reducer

--- frontend/src/reducers/singlePlayerReducer.js
+++ frontend/src/reducers/singlePlayerReducer.js
@@ -1,13 +1,17 @@
-import { FETCH_PLAYERS } from "../config/action-types";
+import { FETCH_PLAYERS, FETCH_SINGLEPLAYER_BOARDS } from "../config/action-types";
 
 const initialState = {
 	players: [],
+	boards: [],
 };
 
 const SinglePlayerReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case FETCH_PLAYERS: {
 			return {...state, players: action.payload}
+		}
+		case FETCH_SINGLEPLAYER_BOARDS: {
+			return {...state, boards: action.payload}
 		}
 		default: {
 			return state;
