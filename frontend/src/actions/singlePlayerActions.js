e5aae95a5186fc0fc1a5fb004cec74b2bd7eed70
Timbo <blaesche.m@gmail.com>
2019-06-28 14:47:25 +0200

    Adds an action to fetch player boards

--- frontend/src/actions/singlePlayerActions.js
+++ frontend/src/actions/singlePlayerActions.js
@@ -1,3 +1,5 @@
-import { FETCH_PLAYERS } from "../config/action-types";
+import { FETCH_PLAYERS, FETCH_SINGLEPLAYER_BOARDS } from "../config/action-types";
 
 export const fetchPlayers = players => ({type: FETCH_PLAYERS, payload: players});
+
+export const fetchSingleplayerBoards = boards => ({type: FETCH_SINGLEPLAYER_BOARDS, payload: boards});
