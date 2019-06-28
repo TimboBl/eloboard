e4d4c1e10d07bbad8873065e5c4e00b81c2fc849
Timbo <blaesche.m@gmail.com>
2019-06-28 10:43:05 +0200

    Build a non finished footer

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+	<footer>
+		<a href={"mailto: blaesche.m@gmail.com"}>Contact me</a>
+		<a>Imprint</a>
+		<a>Privacy Notice</a>
+	</footer>
+);
+
+export default Footer;
