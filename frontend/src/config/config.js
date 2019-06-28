ab3bc990c99d98e751c793a16b2f4b9b81513c48
Timbo <blaesche.m@gmail.com>
2019-06-28 14:48:18 +0200

    Adds more routes

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,9 +1,12 @@
 export const BASE_URL = "http://localhost:3001";
-
 export const SCORES = "/board/scores";
-
 export const NEW_PLAYER = "/board/player";
-
 export const UPDATE_SCORE = "/board/scores/";
+export const BOARDS = "/board";
+export const BOARD_SELECT = "/board/select";
+export const ADD_PLAYER = "/board/player";
 
-export const UPDATE_KPI = "/board/update/kpi";
+export const MULTIPLAYER_GAME = "multiplayer";
+export const SINGLE_PLAYER = "singleplayer";
+
+export const SNACKBAR_DURATION = 2500;
