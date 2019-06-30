d8245a0cd5007684dffee76d0d06006b733a3237
Timbo <blaesche.m@gmail.com>
2019-06-30 12:08:57 +0200

    Creates an escape hatch

--- frontend/src/components/Logo/index.js
+++ frontend/src/components/Logo/index.js
@@ -3,11 +3,13 @@ import React from "react";
 const logo = require("../../images/logo.png");
 
 const Logo = () => (
-	<div style={{marginTop: "-17%"}}>
-		<img src={logo} alt={"Logo"} width={"150px"} style={{marginLeft: "7%"}}/><br/>
-		<span style={{fontSize: "30px", fontWeight: "900"}}>Beerpong Leagues</span>
-		<br/>
-	</div>
+	<a href={"https://timbo.link"} style={{textDecoration: "none", color: "black"}}>
+		<div style={{marginTop: "-17%"}}>
+			<img src={logo} alt={"Logo"} width={"150px"} style={{marginLeft: "7%"}}/><br/>
+			<span style={{fontSize: "30px", fontWeight: "900"}}>Beerpong Leagues</span>
+			<br/>
+		</div>
+	</a>
 );
 
 export default Logo;
